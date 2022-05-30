3ef91d79487bda33e21633ce38333eaeafef6bab
prashantmagare <prashantmagare3010@gmail.com>
2022-05-30 22:39:21 +0530

    "Added - BinarySearchWord"

--- AlgorithmProblem/AlgorithmProblem/Program.cs
+++ AlgorithmProblem/AlgorithmProblem/Program.cs
@@ -1,44 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Day16_17Problems
 {
-    internal class StringEqual
+    internal class BinarySearchWord
     {
-        public static string s, s2, s3, s4;
-        public static void permute(string str, int l, int r)
-
+        public void BinarySearch()
         {
-            if (l == r)
-                Console.WriteLine(str);
-            else
-            {
-                for (int i = l; i <= r; i++)
-                {
-                    str = swap(str, l, i);
-                    permute(str, l + 1, r);
-                    str = swap(str, l, i);
-                }
-            }
 
+            string[] lines = { "Binary", "Search", "Word", "From", "List" };
+            Console.WriteLine("Writing Words into List\n");
+            File.WriteAllLines(@"D:\.net\Day16&17Problems\info.txt", lines);
+            string text = File.ReadAllText(@"D:\.net\Day16&17Problems\info.txt");
+            Console.WriteLine("Before Soritng:\n" + text);
+            Console.WriteLine("After Sorting the Words , They are :\n");
+            Array.Sort(lines, StringComparer.InvariantCulture);
+            Array.ForEach(lines, x => Console.WriteLine(x));
+            Console.WriteLine("Reading Words From File");
+            File.ReadAllLines(@"D:\.net\Day16&17Problems\info.txt");
+            Console.WriteLine("Enter Word to Search");
+            bool value = lines.Contains(Console.ReadLine());
+            Console.WriteLine("If the Word is Found then Print True else False : \n" + value);
         }
-
-        public static string swap(string a, int i, int j)
-
-        {
-            char temp;
-            char[] charArray = a.ToCharArray();
-            temp = charArray[i];
-            charArray[i] = charArray[j];
-            charArray[j] = temp;
-            s = new string(charArray);
-            return s;
-
-        }
-
-
     }
 }
