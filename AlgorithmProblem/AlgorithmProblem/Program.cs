ffab1d8d95741277dd013b11df3fe786bbca0145
prashantmagare <prashantmagare3010@gmail.com>
2022-05-30 22:53:13 +0530

    "Added - UC4_BubbleSorting"

--- AlgorithmProblem/AlgorithmProblem/Program.cs
+++ AlgorithmProblem/AlgorithmProblem/Program.cs
@@ -6,27 +6,39 @@ using System.Threading.Tasks;
 
 namespace Day16_17Problems
 {
-    internal class SortedList
+    internal class BubbleSorting
     {
-        public void Sort()
+        public void BubbleSort()
         {
-            string[] words = new string[5];
-            Console.WriteLine("Enter Words in Array");
-            words[0] = Console.ReadLine();
-            words[1] = Console.ReadLine();
-            words[2] = Console.ReadLine();
-            words[3] = Console.ReadLine();
-            words[4] = Console.ReadLine();
-            Console.WriteLine("Before Sorting\n");
-            for (int i = 0; i < words.Length; i++)
+            int[] intArray = new int[5];
+            Console.WriteLine("Enter 5 Numbers ");
+            intArray[0] = Convert.ToInt32(Console.ReadLine());
+            intArray[1] = Convert.ToInt32(Console.ReadLine());
+            intArray[2] = Convert.ToInt32(Console.ReadLine());
+            intArray[3] = Convert.ToInt32(Console.ReadLine());
+            intArray[4] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before Sorting ");
+            for (int a = 0; a < intArray.Length; a++)
             {
-                Console.WriteLine(words[i] + " ");
+                Console.WriteLine(intArray[a]);
             }
-            Console.WriteLine("After Sorting\n");
-            Array.Sort(words, StringComparer.InvariantCulture);
-            Array.ForEach(words, x => Console.WriteLine(x));
-
-
+            Console.WriteLine("After Sorintg");
+            int temp;
+            for (int j = 0; j <= intArray.Length - 2; j++)
+            {
+                for (int i = 0; i <= intArray.Length - 2; i++)
+                {
+                    if (intArray[i] > intArray[i + 1])
+                    {
+                        temp = intArray[i + 1];
+                        intArray[i + 1] = intArray[i];
+                        intArray[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (int p in intArray)
+                Console.Write(p + "\n");
         }
     }
 }
