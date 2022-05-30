ca9550676b902bffa61070efe5b79b97bfa99682
prashantmagare <prashantmagare3010@gmail.com>
2022-05-30 22:16:57 +0530

    "Added - AlgorithmPrograms"

---
+++ AlgorithmProblem/AlgorithmProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day16_17Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithm Programs ");
+        }
+    }
+}
