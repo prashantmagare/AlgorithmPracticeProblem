3d303bf891ba610a5b3b38a2d76d5dc46b5908c9
prashantmagare <prashantmagare3010@gmail.com>
2022-05-30 22:33:02 +0530

    "Added - UC1_StringEqual"

--- AlgorithmProblem/AlgorithmProblem/Program.cs
+++ AlgorithmProblem/AlgorithmProblem/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Day16_17Problems
 {
-    internal class Program
+    internal class StringEqual
     {
-        static void Main(string[] args)
+        public static string s, s2, s3, s4;
+        public static void permute(string str, int l, int r)
+
         {
-            Console.WriteLine("Algorithm Programs ");
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+
         }
+
+        public static string swap(string a, int i, int j)
+
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            s = new string(charArray);
+            return s;
+
+        }
+
+
     }
 }
